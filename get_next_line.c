a9d580681111d265ec27429309e5920b8ef66ca6
Marco Andree Fernandini Limo <marco-fe@c1r10s6.42madrid.com>
2023-08-03 19:55:47 +0200

    01

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marco-fe <marco-fe@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 12:00:04 by marco-fe          #+#    #+#             */
-/*   Updated: 2023/07/17 16:14:58 by marco-fe         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:58:11 by marco-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,37 @@
 
 #include "get_next_line.h"
 
+char	*read_file(int fd)
+{
+	char	*buff_mid;
+	ssize_t	read_error;
+
+	buff_mid = ft_calloc(BUFFER_SIZE + 1, sizeof (char));
+	if (!buff_mid)
+		return(NULL);
+	read_error = read(fd, buff_mid, BUFFER_SIZE);
+	if (read_error == -1)
+		return (NULL);
+	buff_mid[BUFFER_SIZE + 1] = '\0';
+	return (buff_mid);
+}
+
+void	ind_validator(char *buff_mid)
+{
+	if (ft_strchr(buff_mid, '\n'))
+		print_line();
+	else 
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*buff;
-	char		*line;
+	char	*buff_mid;
+	char	*buff_end;
 
-	if (fd < 0 || BUFFER_SIZE <= 0
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return(NULL);
+	buff_mid = read_file(fd);
+	if (!buff_mid)
+		return(NULL);
+	ind_validator(buff_mid);
 }
